8032a0662198a5bd84851e832876109e7c068e55
petercerhan <petercerhan@gmail.com>
2017-03-20 20:08:37 -0500

    Refine calculations

--- Duromod/DurometerModel.swift
+++ Duromod/DurometerModel.swift
@@ -17,41 +17,42 @@ struct DurometerModel {
      k = Spring constant (N/DH)
      si = Spring sensitivity (displacement per degree of hardness, meters/DH)
      n = Exponent parameter defined by indentor shape: Conical = 2, Round = 3/2, Flat = 1
+     c = Adjustment factor for shape of indentor (1 for flat indentors)
      
      note: DH = "Degree of hardness"
     */
-    typealias Properties = (_F_0: Double, r: Double, p_0: Double, k: Double, si: Double, n: Double)
+    typealias Properties = (_F_0: Double, r: Double, p_0: Double, k: Double, si: Double, n: Double, c: Double)
     
     var scale: Scale
     var properties: Properties {
         get {
             switch scale {
             case .shore:
-                return (_F_0: 0.549, r: 0.000_395, p_0: 0.002_54, k: 0.075_1, si: 0.000_025_4,  n: 1)
+                return (_F_0: 0.549, r: 0.000_395, p_0: 0.002_54, k: 0.075_1, si: 0.000_025_4,  n: 1, c: 1)
             case .a:
-                return (_F_0: 0.550, r: 0.000_395, p_0: 0.002_50, k: 0.075_0, si: 0.000_025, n: 1)
+                return (_F_0: 0.550, r: 0.000_395, p_0: 0.002_50, k: 0.075_0, si: 0.000_025, n: 1, c: 1)
             case .b:
-                return (_F_0: 0.550, r: 0.000_670, p_0: 0.002_50, k: 0.075_0, si: 0.000_025, n: 2)
+                return (_F_0: 0.550, r: 0.000_670, p_0: 0.002_50, k: 0.075_0, si: 0.000_025, n: 2, c: 3.14)
             case .c:
-                return (_F_0: 0, r: 0.000_395, p_0: 0.002_50, k: 0.444_5, si: 0.000_025, n: 1)
+                return (_F_0: 0, r: 0.000_395, p_0: 0.002_50, k: 0.444_5, si: 0.000_025, n: 1, c: 1)
             case .d:
-                return (_F_0: 0, r: 0.000_670, p_0: 0.002_50, k: 0.444_5, si: 0.000_025, n: 2)
+                return (_F_0: 0, r: 0.000_670, p_0: 0.002_50, k: 0.444_5, si: 0.000_025, n: 2, c: 3.14)
             case .o:
-                return (_F_0: 0.55, r: 0.001_19, p_0: 0.002_50, k: 0.075_0, si: 0.000_025, n: 1.5)
+                return (_F_0: 0.55, r: 0.001_19, p_0: 0.002_50, k: 0.075_0, si: 0.000_025, n: 1.5, c: 1.03)
             case .do_:
-                return (_F_0: 0, r: 0.001_19, p_0: 0.002_50, k: 0.444_5, si: 0.000_025, n: 1.5)
+                return (_F_0: 0, r: 0.001_19, p_0: 0.002_50, k: 0.444_5, si: 0.000_025, n: 1.5, c: 1.03)
             case .oo:
-                return (_F_0: 0.203, r: 0.001_19, p_0: 0.002_50, k: 0.009_08, si: 0.000_025, n: 1.5)
+                return (_F_0: 0.203, r: 0.001_19, p_0: 0.002_50, k: 0.009_08, si: 0.000_025, n: 1.5, c: 1.03)
             case .m:
-                return (_F_0: 0.324, r: 0.000_394, p_0: 0.001_25, k: 0.004_40, si: 0.000_0125, n: 2)
+                return (_F_0: 0.324, r: 0.000_394, p_0: 0.001_25, k: 0.004_40, si: 0.000_0125, n: 2, c: 3.69)
             case .e:
-                return (_F_0: 0.55, r: 0.002_5, p_0: 0.002_50, k: 0.075_0, si: 0.000_025, n: 1.5)
+                return (_F_0: 0.55, r: 0.002_5, p_0: 0.002_50, k: 0.075_0, si: 0.000_025, n: 1.5, c: 1.5)
             case .ooo:
-                return (_F_0: 0.203, r: 0.006_35, p_0: 0.002_50, k: 0.009_08, si: 0.000_025, n: 1.5)
+                return (_F_0: 0.203, r: 0.006_35, p_0: 0.002_50, k: 0.009_08, si: 0.000_025, n: 1.5, c: 2.39)
             case .ooo_s:
-                return (_F_0: 0.167, r: 0.010_7, p_0: 0.005_0, k: 0.017_65, si: 0.000_05, n: 1.5)
+                return (_F_0: 0.167, r: 0.010_7, p_0: 0.005_0, k: 0.017_65, si: 0.000_05, n: 1.5, c: 2.19)
             case .cf:
-                return (_F_0: 1.098_7, r: 0.006_35, p_0: 0.005_0, k: 0.882_9, si: 0.000_05, n: 1.5)
+                return (_F_0: 1.098_7, r: 0.006_35, p_0: 0.005_0, k: 0.882_9, si: 0.000_05, n: 1.5, c: 1.69)
             }
         }
     }
@@ -66,13 +67,14 @@ struct DurometerModel {
         let k = properties.k
         let si = properties.si
         let n = properties.n
+        let factor = properties.c
         
         let a = (3*_F_0) / (8*r*p_0)
         let b = (3*k) / (8*r*si)
         let c = a + b*_H
         let d: Double = pow((1 - _H), -n)
         
-        return c*d
+        return c*d*factor
     }
     
 }
